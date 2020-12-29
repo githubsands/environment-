9305ab46258c0f368b3b791175cf4b1b9bc51b10
work <26752232+githubsands@users.noreply.github.com>
2020-12-28 20:24:32 -0700

    update

---
+++ setup-env.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/tcltk/tcl
+cd tcl
+./configure
+sudo make install
+
+git clone https://github.com/ershov/tmux
+sh autogen.sh
+./configure
+make
+
+brew install neovim
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
