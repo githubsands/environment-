7aaf93f315eb0a68f5274be77977090b5e2b29e6
Ryan Vacek <ryvace@protonmail.com>
2022-06-23 13:23:24 -0600

    update

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 rustup component add clippy
 rustup component add rustfmt
 rustup component add rust-src
+
+sh <(curl -L https://nixos.org/nix/install)
 brew install npm
 
- git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
- git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git config --global user.email ${EMAIL}
 git config --global user.name ${NAME}
