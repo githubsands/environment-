1766d411892c9718ac9185e6f831053b855b0334
Ryan Vacek <ryvace@protonmail.com>
2022-08-25 10:42:38 -0600

    Adds alpine machine to tmux; updates path of searchh alpine machine script

--- tools/alpinemachine/search-alpine-machine.sh
+++ tools/alpinemachine/search-alpine-machine.sh
@@ -3,7 +3,7 @@
 # search-alpine-machine allows us to open up an alpine machine to search
 # for specific packages on any OS or platform w/o having to use the web
 
-docker build -t alpine-machine -f Dockerfile.alpinemachine .
+docker build -t alpine-machine -f ~/environment-/tools/alpinemachine/Dockerfile.alpinemachine .
 docker run --name apk-search alpine-machine &
 docker exec apk-search apk-update
 docker exec -it apk-search zsh
